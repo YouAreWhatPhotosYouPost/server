8ca8a35aedd14190a9ca4ffd6571b2054e3c32f5
Zena <zena.vilenia@gmail.com>
2018-04-05 22:07:10 +0700

    faceanalyze

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,3 +1,6 @@
+const image = require('../models/image.model')
+const mongoose = require('mongoose');
+
 module.exports = {
     homePage: (req, res) => {
         
@@ -7,5 +10,30 @@ module.exports = {
     },
     register: (req, res) => {
         
+    },
+    faceAnalyze: (req, res) => {
+        const emotions = req.body.data[0].faceAttributes.emotion;
+        const urlImage = req.body.data[0].urlImage;
+        console.log("emotions- ", emotions)
+        console.log("url- ", urlImage)
+
+        let newImage = new image({
+            emotions, urlImage
+        })
+
+        console.log("new image- ", newImage)
+
+        newImage.save((err, result) => {
+            if (!err) {
+                res.status(201).send({
+                    message: 'face analyze saved',
+                    data: result
+                })
+            } else {
+                res.status(400).send({
+                    message: err.message
+                })
+            }
+        })
     }
 }
