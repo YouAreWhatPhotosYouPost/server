c37553d694073d5688621391816a09f365124f9f
Dani Damara <danny12march@gmail.com>
2018-04-06 00:48:04 +0700

    Dari client udah bisa save music Rekomendasi, tapi kalo button di client di click terus bakal ke simpen di database sesuai jumlah klik

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
 const {homePage, login, register} = require('../controllers/index.controller')
+const {saveMusicRec, showAllMusic, deleteMusicRec} = require('../controllers/music.controller')
 
 router
     .get('/', homePage)
     .post('/login', login)
     .post('/register', register)
+    .get('/music', showAllMusic)
+    .post('/music/save', saveMusicRec)
+    .delete('/music/:id', deleteMusicRec)
+
 
 module.exports = router
