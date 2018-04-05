2910d32c8f55bbfed29566767504cafac9c85ed1
Zena <zena.vilenia@gmail.com>
2018-04-05 22:07:10 +0700

    faceanalyze

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
-const {homePage, login, register} = require('../controllers/index.controller')
+const {homePage, login, register, faceAnalyze} = require('../controllers/index.controller')
 
 router
     .get('/', homePage)
     .post('/login', login)
     .post('/register', register)
-
+    .post('/faceAnalyze', faceAnalyze)
 module.exports = router
