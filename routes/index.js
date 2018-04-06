61e7700507c2a88a935ad569df6343b968f49543
Dani Damara <danny12march@gmail.com>
2018-04-06 08:16:39 +0700

    Fix conflict branch development with itunesMusic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router()
 const {homePage, login, register, faceAnalyze} = require('../controllers/index.controller')
+const {homePage, login, register} = require('../controllers/index.controller')
+const {saveMusicRec, showAllMusic, deleteMusicRec} = require('../controllers/music.controller')
 
 router
     .get('/', homePage)
     .post('/login', login)
     .post('/register', register)
     .post('/faceAnalyze', faceAnalyze)
+    .get('/music', showAllMusic)
+    .post('/music/save', saveMusicRec)
+    .delete('/music/:id', deleteMusicRec)
+
+
 module.exports = router
